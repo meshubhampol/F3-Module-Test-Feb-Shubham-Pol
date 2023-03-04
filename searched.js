bdf98fc5dc593d48a8e1b91f4f10fbee1065003d
meshubhampol <maheshpol9@gmail.com>
2023-03-04 23:31:10 +0530

    minor changes

--- searched.js
+++ searched.js
@@ -1,5 +1,4 @@
 let index =JSON.parse(localStorage.getItem('index'));
-// console.log(index);
 
 let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
@@ -7,38 +6,33 @@ let historyData = JSON.parse(localStorage.getItem('history'));
 let query = historyData[index].query;
 query = query.split(" ").join('+');
 url= url+query;
-let data=[];
-fetch(url).then((response)=> response.json()).then(
-    (result)=> {
-        console.log(result);
-        data=result.items;
-        console.log(data);
-    }
-)
-
 let container = document.getElementById('result-table');
 
 let sr=1;
 let result;
-historyData.map((item) => {
-    let row = document.createElement('div');
-    row.classList.add('item-row');
-    row.innerHTML = `
-        <div>${sr++}. ${item.query}</div>
-        <div>Searcheed On: ${item.date} at ${item.time}</div>`
-    container.appendChild(row);
-    console.log(sr-2);
-    if(sr-2 === index) {
-        result = document.createElement('div');
-        result.classList.add('result-container');
-        mapData();
-        console.log(result);
-    }
-});
 
-function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
+let data=[];
+fetch(url).then((response)=> response.json()).then(
+    (result)=> {
+        data=result.items;
+        console.log('data',data);
+    }
+).then(()=> {
+    historyData.map((item) => {
+        let row = document.createElement('div');
+        row.classList.add('item-row');
+        row.innerHTML = `
+            <div>${sr++}. ${item.query}</div>
+            <div>Searcheed On: ${item.date} at ${item.time}</div>`
+        container.appendChild(row);
+
+        if(sr-2 === index) {
+            result = document.createElement('div');
+            result.id='result-container';
+            mapData();
+        }
+    });
+});
 
 function mapData() {
     data.map(showBooks);
@@ -69,6 +63,6 @@ function showBooks(book) {
             <div>
                 <button class="buy-now">Buy Now</button>
             </div>`
-    
+
     result.appendChild(bookItem);
 }
