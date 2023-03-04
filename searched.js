8092ca5536f46342a9be70bde4748db8f976e234
meshubhampol <maheshpol9@gmail.com>
2023-03-04 23:58:08 +0530

    search enabled for every searched item

--- searched.js
+++ searched.js
@@ -1,23 +1,23 @@
-let index =JSON.parse(localStorage.getItem('index'));
+let index = JSON.parse(localStorage.getItem('index'));
 
 let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
 let historyData = JSON.parse(localStorage.getItem('history'));
 let query = historyData[index].query;
 query = query.split(" ").join('+');
-url= url+query;
+url = url + query;
 let container = document.getElementById('result-table');
 
-let sr=1;
+let sr = 1;
 let result;
 
-let data=[];
-fetch(url).then((response)=> response.json()).then(
-    (result)=> {
-        data=result.items;
-        console.log('data',data);
+let data = [];
+fetch(url).then((response) => response.json()).then(
+    (result) => {
+        data = result.items;
+        console.log('data', data);
     }
-).then(()=> {
+).then(() => {
     historyData.map((item) => {
         let row = document.createElement('div');
         row.classList.add('item-row');
@@ -26,11 +26,21 @@ fetch(url).then((response)=> response.json()).then(
             <div>Searcheed On: ${item.date} at ${item.time}</div>`
         container.appendChild(row);
 
-        if(sr-2 === index) {
+        if (sr - 2 === index) {
+            row.scrollIntoView();
             result = document.createElement('div');
-            result.id='result-container';
+            result.id = 'result-container';
             mapData();
         }
+        let rows = document.getElementsByClassName('item-row');
+        console.log(rows[0]);
+        for (let i = 0; i < rows.length; i++) {
+            rows[i].addEventListener('click', () => {
+                console.log(i);
+                localStorage.setItem('index', JSON.stringify(i));
+                window.location.reload();
+            })
+        };
     });
 });
 
@@ -43,8 +53,8 @@ function mapData() {
 function showBooks(book) {
     let title = book.volumeInfo.title;
     let author = "";
-    if(book.volumeInfo.authors) {
-        author=book.volumeInfo.authors[0];
+    if (book.volumeInfo.authors) {
+        author = book.volumeInfo.authors[0];
     }
 
     let bookItem = document.createElement('div');
@@ -52,7 +62,7 @@ function showBooks(book) {
     let thumbnail = book.volumeInfo.imageLinks.thumbnail;
     let page = book.volumeInfo.pageCount;
     let publisher = book.volumeInfo.publisher;
-    bookItem.innerHTML=`
+    bookItem.innerHTML = `
             <img src=${thumbnail}
                 alt="book">
             <div class="text-section">
@@ -66,3 +76,4 @@ function showBooks(book) {
 
     result.appendChild(bookItem);
 }
+
